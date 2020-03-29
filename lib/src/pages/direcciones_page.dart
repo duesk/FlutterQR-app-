3f8260e59268616379095d0278570c16059c7ca0
Rafael Lopez Barajas <ideas.rafael@gmail.com>
2020-03-29 01:45:49 -0600

    BottomNavigatorBar

---
+++ lib/src/pages/direcciones_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DireccionPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Direcciones  Page"),
+    );
+  }
+}
